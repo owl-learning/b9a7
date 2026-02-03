0e536118c103c51c85f67cef305b91029a4cbf2a
skl <skl@skl.sl>
2026-02-03 14:29:44 +0600

    add nav bar

--- b9a7/src/componet/navbar/Nav.jsx
+++ b9a7/src/componet/navbar/Nav.jsx
@@ -1,7 +1,28 @@
+import { FaRegUserCircle } from "react-icons/fa";
 const Nav = () => {
   return (
     <>
-      
+      <div className="w-11/12 mx-auto py-4">
+        <nav className="flex justify-between items-center">
+          <div className="text-4xl">Recipe Calories</div>
+          <div className="">
+            <ul className="flex ">
+              <li className="mx-3 font-bold">Home</li>
+              <li className="mx-3 font-bold">Recipes</li>
+              <li className="mx-3 font-bold">About</li>
+              <li className="mx-3 font-bold">Location</li>
+            </ul>
+          </div>
+          <div className="flex">
+            <input
+              className="outline-1 outline-amber-50 rounded-2xl px-3"
+              type="text"
+              placeholder="Search"
+            />
+            <FaRegUserCircle className="text-3xl ml-3"></FaRegUserCircle>
+          </div>
+        </nav>
+      </div>
     </>
   );
 };
