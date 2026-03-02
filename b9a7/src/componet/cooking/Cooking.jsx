7569e91d9a288540d90f96f74e682160a601a776
skl <skl@skl.sl>
2026-03-02 15:52:09 +0600

    add button

--- b9a7/src/componet/cooking/Cooking.jsx
+++ b9a7/src/componet/cooking/Cooking.jsx
@@ -5,10 +5,11 @@ const Cooking = ({ cookData }) => {
     return (
         <>
             <div className="grid grid-cols-4 gap-x-8  justify-around text-center items-center bg-amber-100 mt-2.5 mb-2.5  
-             font-semibold text-black">
+             font-semibold text-black p-2 rounded-2xl">
                 <h1>{recipe_name}</h1>
                 <h1>{calories}</h1>
                 <h1>{preparing_time}</h1>
+                <button className="py-1.5 text-xl bg-green-500 rounded-full text-black font-bold " >Preparing</button>
         </div>
       </>
     );
