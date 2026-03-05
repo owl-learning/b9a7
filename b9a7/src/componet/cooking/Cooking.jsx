7fbc881dde4c794b287eeef893547ae05a172154
skl <skl@skl.sl>
2026-03-05 16:43:56 +0600

    pass all data just need to designe

--- b9a7/src/componet/cooking/Cooking.jsx
+++ b9a7/src/componet/cooking/Cooking.jsx
@@ -4,8 +4,8 @@ const Cooking = ({ cookData }) => {
     
     const { recipe_name } = cookData;
     return (
-        <div>
-            <h1 className="text-black">{ recipe_name}</h1>
+        <div className="text-black">
+            <h1 className="">{ recipe_name}</h1>
         </div>
     );
 };
